c866c939069db99873a86cb88565bfb2136eed1b
Lucash00 <lucas.moreno.dev@gmail.com>
2024-12-12 19:56:09 +0100

    realizacion de ejrcicio, preparado para subir a Heroku

--- index.js
+++ index.js
@@ -4,29 +4,53 @@
 // init project
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// enable CORS so that your API is remotely testable by FCC
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+// static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// home route
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// first API endpoint
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// Timestamp API
+app.get("/api/:date?", (req, res) => {
+  let dateParam = req.params.date;
 
+  if (!dateParam) {
+    const now = new Date();
+    return res.json({
+      unix: now.getTime(),
+      utc: now.toUTCString()
+    });
+  }
 
-// Listen on port set in environment variable or default to 3000
+  if (!isNaN(dateParam)) {
+    dateParam = parseInt(dateParam, 10);
+  }
+
+  const date = new Date(dateParam);
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+// start server
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
